606b39b4ef23b78f5234f64c1749ad029c152834
thiennguyendev <thiennguyen.mark@gmail.com>
2019-03-08 10:13:58 +0700

    Define sing up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
   post '/auth/login', to: 'authentication#login'
+  post '/auth/signup', to: 'users#create'
   get '/*a', to: 'application#not_found'
 end
