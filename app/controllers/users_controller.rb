cdd38b12fb1822100c9e732178007f0280dadbf0
thiennguyendev <thiennguyen.mark@gmail.com>
2019-03-09 14:04:59 +0700

    Refactor coding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: {
+          email: @user.mail
+        }, status: :created
     else
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
