0c479faf1eab5d985c9a72ac3b6463446d0da5d1
thiennguyendev <thiennguyen.mark@gmail.com>
2019-03-16 15:04:38 +0700

    Refactor coding

--- test/models/detail_course_test.rb
+++ test/models/detail_course_test.rb
@@ -1,7 +1,3 @@
 require 'test_helper'
 
-class DetailCourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+class DetailCourseTest < ActiveSupport::TestCase; end
