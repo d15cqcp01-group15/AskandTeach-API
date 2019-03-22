ac531bf73fed0f5f5a33c9be2268eb9d223fa80e
thiennguyendev <thiennguyen.mark@gmail.com>
2019-03-22 09:42:35 +0700

    Add avatar uploader

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :detail_courses
   resources :courses
   resources :users, param: :_username
+
   post '/auth/login', to: 'authentication#login'
   post '/auth/signup', to: 'users#create'
+
   get '/*a', to: 'application#not_found'
 end
