6eb475c47c71cc2390adb491adf73243975d100a
thiennguyendev <thiennguyen.mark@gmail.com>
2019-04-01 19:11:56 +0700

    some change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_022707) do
+ActiveRecord::Schema.define(version: 2019_04_01_114756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,16 @@ ActiveRecord::Schema.define(version: 2019_03_22_022707) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "topic_type"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -77,4 +87,5 @@ ActiveRecord::Schema.define(version: 2019_03_22_022707) do
   add_foreign_key "detail_events", "events"
   add_foreign_key "detail_events", "users"
   add_foreign_key "events", "users"
+  add_foreign_key "topics", "users"
 end
