ecb69a11a0e70796a29c8da659c43cfbfaff45fd
thiennguyendev <thiennguyen.mark@gmail.com>
2019-04-02 21:17:02 +0700

    change core

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module TTDApi
     config.load_defaults 5.2
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000'
+        origins '*'
         resource '*', headers: :any, methods: :any
       end
     end
